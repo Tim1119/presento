a63a6882aa929ae97ce38cacc9a2a00ab54d2777
tim1119 <timothyinioluwa@gmail.com>
2024-02-25 02:26:21 +0100

    setup django for railway

--- presento/settings/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'presento.base')
-
-app = Celery('presento')
-app.config_from_object('django.conf:base', namespace='CELERY')
-app.autodiscover_tasks()
